403faa6af90dfa00370cb2daf7ade293a60a10ae
Isaac Oduh <isaacoduh@IOs-MacBook-Pro.local>
2023-08-18 15:14:10 +0100

    increase token expiry time, return projects that a customer has purchased

--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -1,16 +1,45 @@
-const express = require('express');
-const authMiddleware = require('../middleware/auth');
-const User = require('../models/user.model');
+const express = require("express");
+const authMiddleware = require("../middleware/auth");
+const User = require("../models/user.model");
+const Order = require("../models/order.model");
 
 const profile = async (req, res) => {
-    try {
-        const user = await User.findById(req.userId);
-        res.json(user);
-    } catch (error) {
-        res.status(500).json({error: error.message});
-    }
+  try {
+    const selectedFields = "orderReferenceId";
+    const user = await User.findById(req.userId);
+    const history = await Order.aggregate([
+      { $match: { user: user._id } },
+      { $unwind: "$orderItems" },
+      {
+        $group: {
+          _id: "$orderItems.project",
+          orders: { $addToSet: "$_id" },
+          quantity: { $sum: "$orderItems.quantity" },
+        },
+      },
+      {
+        $lookup: {
+          from: "projects",
+          localField: "_id",
+          foreignField: "_id",
+          as: "projectInfo",
+        },
+      },
+      {
+        $project: {
+          projectId: "$_id",
+          title: { $arrayElemAt: ["$projectInfo.title", 0] },
+          orders: 1,
+          quantity: 2,
+        },
+      },
+    ]);
+    res.status(200).json({ user, history });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 module.exports = {
-    profile
-}
+  profile,
+};
