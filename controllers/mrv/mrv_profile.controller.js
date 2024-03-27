2524edb39d08ac7795743fc396968f269144ef6f
Isaac Oduh <isaacoduh@IOs-MacBook-Pro.local>
2024-03-27 15:09:24 +0000

    profile: update profile with image

--- controllers/mrv/mrv_profile.controller.js
+++ controllers/mrv/mrv_profile.controller.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const authMiddleware = require("../../middleware/auth");
 const MrvUser = require("../../models/mrv_user.model");
+const cloudinary = require("../../utils/cloudinary");
 
 const getProfile = async (req, res) => {
   try {
@@ -10,6 +11,7 @@ const getProfile = async (req, res) => {
       _id: mrvUser._id,
       firstname: mrvUser.firstname,
       lastname: mrvUser.lastname,
+      email: mrvUser.email,
       profileImageUrl: mrvUser.profileImageUrl,
       isOnboardingComplete: mrvUser.isOnboardingComplete,
     };
@@ -19,4 +21,44 @@ const getProfile = async (req, res) => {
   }
 };
 
-module.exports = { getProfile };
+const updateProfile = async (req, res) => {
+  try {
+    const { firstname, lastname, profileImage } = req.body;
+
+    const mrvUser = await MrvUser.findById(req.userId);
+    if (!mrvUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Update firstname and lastname
+    mrvUser.firstname = firstname;
+    mrvUser.lastname = lastname;
+
+    if (profileImage) {
+      const uploadImage = await cloudinary.v2.uploader.upload(profileImage, {});
+      mrvUser.profileImageUrl = uploadImage.secure_url;
+    }
+
+    await mrvUser.save();
+    // const updateUser = await MrvUser.findByIdAndUpdate(req.userId, {
+    //   firstname,
+    //   lastname,
+    // });
+    // await updateUser.save();
+    // const result = {
+    //   _id: updateUser._id,
+    //   firstname: updateUser.firstname,
+    //   lastname: updateUser.lastname,
+    //   email: updateUser.email,
+    //   profileImageUrl: updateUser.profileImageUrl,
+    //   isOnboardingComplete: updateUser.isOnboardingComplete,
+    // };
+
+    res.status(200).json({ message: "Profile updated successfully" });
+    // res.status(200).json({ mrvUser: result });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { getProfile, updateProfile };
