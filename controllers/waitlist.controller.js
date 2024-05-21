7cb63cb8083919452ce013d4274e472bce2613c4
Demi Babajide <dbabajide@opay-inc.com>
2024-05-21 14:03:26 +0100

    Fixed waitlist error

--- controllers/waitlist.controller.js
+++ controllers/waitlist.controller.js
@@ -14,15 +14,21 @@ const addUsertoWaitlist = async (req, res) => {
     const { full_name, phone_number, email, farm_country, farm_size } =
       req.body;
 
-    const waitlistUser = new Waitlist({
-      full_name,
-      phone_number,
-      email,
-      farm_country,
-      farm_size,
-    });
-    await waitlistUser.save();
-    res.status(201).json({ message: "Added to waitlist" });
+    if (Waitlist.findOne({ email: req.email })) {
+      res.status(400).json({ message: "User already exists" });
+    }
+
+    else {
+      const waitlistUser = new Waitlist({
+        full_name,
+        phone_number,
+        email,
+        farm_country,
+        farm_size,
+      });
+      await waitlistUser.save();
+      res.status(201).json({ message: "Added to waitlist" });
+    }
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
