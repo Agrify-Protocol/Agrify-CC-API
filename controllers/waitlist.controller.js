8a1d8ec835c89a2bb46f64613671cd61bc08a032
Isaac Oduh <isaacoduh@IOs-MacBook-Pro.local>
2024-05-22 13:19:47 +0100

    fix email duplicate

--- controllers/waitlist.controller.js
+++ controllers/waitlist.controller.js
@@ -14,22 +14,27 @@ const addUsertoWaitlist = async (req, res) => {
     const { full_name, phone_number, email, farm_country, farm_size } =
       req.body;
 
-    if (Waitlist.findOne({ email: req.email })) {
-      res.status(400).json({ message: "User already exists" });
+    const userExists = await Waitlist.findOne({ email: req.email });
+    if (userExists) {
+      throw new Error("User already exist ");
     }
 
-    else {
-      const waitlistUser = new Waitlist({
-        full_name,
-        phone_number,
-        email,
-        farm_country,
-        farm_size,
-      });
-      await waitlistUser.save();
-      res.status(201).json({ message: "Added to waitlist" });
-    }
+    const result = await Waitlist.create({
+      full_name,
+      phone_number,
+      email,
+      farm_country,
+      farm_size,
+    });
+    await result.save();
+    res.status(201).json({ message: "User Added to Waitlist" });
   } catch (error) {
+    if (error.code && error.code === 11000) {
+      const message = `${Object.keys(
+        error.keyValue
+      )} field must be unique. A record with the provided unique field already exists`;
+      return res.status(500).json({ message });
+    }
     res.status(500).json({ error: error.message });
   }
 };
