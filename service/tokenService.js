f9b39c4db35b02540abf94a6b932097a998c67a3
Demi Babajide <dbabajide@opay-inc.com>
2024-05-24 14:07:11 +0100

    Token creation

--- service/tokenService.js
+++ service/tokenService.js
@@ -3,12 +3,7 @@ const hederaService = require("../hedera/service/createToken.js");
 
 const createToken = async (projectId, tokenName, tokenOwner, initialSupply) => {
 	try {
-		// const tokenName = req.tokenName;  
 		const tokenSymbol = projectId+"-AGT";  
-		// const initialSupply = req.initialSupply;  
-		// const tokenOwner = "664e048797ed863403c58463"; req.userId;
-		// const projectId = "123";
-		// const tokenId = "12345";
 		const tokenId = await hederaService.createHederaToken(tokenName, tokenSymbol, initialSupply);
 		if (!tokenId) throw new Error("Error creating token");
 		const token = await Token.create({
