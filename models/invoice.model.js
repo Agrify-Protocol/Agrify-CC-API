135f33efbc9b9098ef78d97710fcfa0ce8740f8b
Isaac Oduh <isaacoduh@IOs-MacBook-Pro.local>
2024-06-28 11:13:41 +0100

    implement invoice feature

---
+++ models/invoice.model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const invoiceSchema = new Schema(
+  {
+    clientName: { type: String, required: true },
+    paymentDueDate: { type: Date, required: true },
+    phoneNumber: { type: String, required: true },
+    projectId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project",
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    quantity: { type: Number, required: true },
+    amount: { type: Number, required: true },
+    status: {
+      type: String,
+      enum: ["pending", "cancelled", "paid"],
+      default: "pending",
+    },
+    invoiceNo: { type: String, required: true, unique: true },
+    address: { type: String, required: true },
+    contactNo: { type: String, required: true },
+    issuedOn: { type: Date, default: Date.now },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Invoice", invoiceSchema);
