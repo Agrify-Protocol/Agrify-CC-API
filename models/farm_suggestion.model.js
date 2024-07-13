4bd38a472bba61f917f88ace181088bca62ede7b
Isaac Oduh <isaacoduh@IOs-MacBook-Pro.local>
2024-07-13 16:23:23 +0100

    add timestamps to farm suggestions

--- models/farm_suggestion.model.js
+++ models/farm_suggestion.model.js
@@ -8,22 +8,29 @@ const taskSchema = new Schema(
 
 const monthSchema = new Schema({ Tasks: [taskSchema] }, { _id: false });
 
-const farmSuggestionSchema = new Schema({
-  PeriodStart: { type: Date, required: true },
-  PeriodEnd: { type: Date, required: true },
-  // FarmerId: {
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   ref: "MrvUser",
-  //   required: true,
-  // },
-  FarmId: { type: mongoose.Schema.Types.ObjectId, ref: "Farm", required: true },
-  FarmerName: { type: String, required: true },
-  FarmScore: { type: Number, required: true },
-  RecommendationsIntro: { type: String, required: true },
-  Month1: { type: monthSchema, required: true },
-  Month2: { type: monthSchema, required: true },
-  Month3: { type: monthSchema, required: true },
-  RecommendationsSummary: { type: String, required: true },
-});
+const farmSuggestionSchema = new Schema(
+  {
+    PeriodStart: { type: Date, required: true },
+    PeriodEnd: { type: Date, required: true },
+    // FarmerId: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   ref: "MrvUser",
+    //   required: true,
+    // },
+    FarmId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Farm",
+      required: true,
+    },
+    FarmerName: { type: String, required: true },
+    FarmScore: { type: Number, required: true },
+    RecommendationsIntro: { type: String, required: true },
+    Month1: { type: monthSchema, required: true },
+    Month2: { type: monthSchema, required: true },
+    Month3: { type: monthSchema, required: true },
+    RecommendationsSummary: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("FarmSuggestion", farmSuggestionSchema);
