7cbdde17fdeff299d62ac09ec9e475b6c68ff3d7
Isaac Oduh <ufuomaoduh@gmail.com>
2024-07-23 12:17:06 +0100

    implement overview information endpoint

--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const authMiddleware = require("../middleware/auth");
 const User = require("../models/user.model");
 const Order = require("../models/order.model");
+const Purchase = require("../models/purchase.model");
 
 const profile = async (req, res) => {
   try {
@@ -40,6 +42,77 @@ const profile = async (req, res) => {
   }
 };
 
+const overview = async (req, res) => {
+  try {
+    // get total projects funded
+    // using purchases for now
+
+    const userId = req.userId;
+
+    const purchases = await Purchase.find({ userId: req.userId }).populate(
+      "projectId"
+    );
+
+    const uniqueProjects = new Set(
+      purchases.map((purchase) => purchase.projectId.toString())
+    );
+
+    const totalProjectsFunded = uniqueProjects.size;
+    const totalTonnes = purchases.reduce(
+      (sum, purchase) => sum + purchase.tonnes,
+      0
+    );
+
+    // total amounts from invoices
+    const invoiceAmounts = await Purchase.aggregate([
+      {
+        $match: {
+          userId: new mongoose.Types.ObjectId(userId),
+          invoiceId: { $exists: true, $ne: null },
+        },
+      },
+      {
+        $lookup: {
+          from: "invoices",
+          localField: "invoiceId",
+          foreignField: "_id",
+          as: "invoice",
+        },
+      },
+      { $unwind: "$invoice" },
+      {
+        $group: { _id: null, totalInvoiceAmount: { $sum: "$invoice.amount" } },
+      },
+      { $project: { _id: 0, totalInvoiceAmount: 1 } },
+    ]);
+
+    const totalInvoiceAmount =
+      invoiceAmounts.length > 0 ? invoiceAmounts[0].totalInvoiceAmount : 0;
+    const totalAmount = totalInvoiceAmount;
+
+    const projectsFunded = purchases.map((purchase) => ({
+      title: purchase.projectId.title,
+      location: purchase.projectId.countryOfOrigin,
+      purchaseType: purchase.purchaseType.toUpperCase,
+      startDate: new Date(
+        purchase.projectId.creditStartDate
+      ).toLocaleDateString("en-US"),
+    }));
+    const data = {
+      totalProjectsFunded,
+      totalTonnes,
+      totalAmount,
+      projectsFunded,
+    };
+
+    res.status(200).json({ data });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
 module.exports = {
   profile,
+  overview,
 };
--- routes/profile.route.js
+++ routes/profile.route.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const {profile} = require('../controllers/profile.controller');
-const authMiddleware = require('../middleware/auth');
+const express = require("express");
+const { profile, overview } = require("../controllers/profile.controller");
+const authMiddleware = require("../middleware/auth");
 const router = express.Router();
 
-router.get('/profile', authMiddleware, profile);
+router.get("/profile", authMiddleware, profile);
+router.get("/profile/overview", authMiddleware, overview);
 
 module.exports = router;
