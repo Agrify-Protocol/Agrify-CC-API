9882eaa1ff4560876430374c7ce8042554deaca4
Isaac Oduh <ufuomaoduh@gmail.com>
2024-07-23 12:37:20 +0100

    implement basic reports

--- routes/profile.route.js
+++ routes/profile.route.js
@@ -1,9 +1,18 @@
 const express = require("express");
-const { profile, overview } = require("../controllers/profile.controller");
+const {
+  profile,
+  overview,
+  createReport,
+  myReports,
+  reportDetails,
+} = require("../controllers/profile.controller");
 const authMiddleware = require("../middleware/auth");
 const router = express.Router();
 
 router.get("/profile", authMiddleware, profile);
 router.get("/profile/overview", authMiddleware, overview);
+router.post("/profile/reports/create", authMiddleware, createReport);
+router.get("/profile/reports", authMiddleware, myReports);
+router.get("/profile/reports/:reportId", authMiddleware, reportDetails);
 
 module.exports = router;
